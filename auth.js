3c55239e3d76934cd740d0c0a032823996b4fba9
Juan Fernandez <juanfernandezdeorta@gmail.com>
2023-01-31 14:34:15 +0100

    Login en archivo separado

---
+++ auth.js
@@ -0,0 +1,72 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-auth.js";
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB6MPkwmFCZQlWcXbDy5zYECJCZV6429dc",
+    authDomain: "todo-list-juan.firebaseapp.com",
+    projectId: "todo-list-juan",
+    storageBucket: "todo-list-juan.appspot.com",
+    messagingSenderId: "290993083560",
+    appId: "1:290993083560:web:d908e94d5bb8d1478d1e9d"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
+export const provider = new GoogleAuthProvider()
+export const user = ""
+
+//Funcion de login
+export async function login() {
+    try {
+        await signInWithPopup(auth, provider).then((result) => {
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+            // The signed-in user info.
+            const user = result.user;
+            // Quitamos el boton de login y mostramos el de logOut
+            document.getElementById("boton-login").classList.replace("flex", "hidden")
+            document.getElementById("boton-logout").classList.replace("hidden", "flex")
+            return user
+        }).catch((error) => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            const email = error.customData.email;
+            // The AuthCredential type that was used.
+            const credential = GoogleAuthProvider.credentialFromError(error);
+            // ...
+        });
+
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+//Funcion de logout
+export function logOut() {
+
+    signOut(auth).then(() => {
+        console.log("LogOut succesful")
+        //Quitamos el usuario.
+        document.getElementById("user").innerHTML = ""
+        // Quitamos el boton de logout y mostramos el de login.
+        document.getElementById("boton-logout").classList.replace("flex", "hidden")
+        document.getElementById("boton-login").classList.replace("hidden", "flex")
+        //Borramos las tasks del usuario.
+        document.getElementById("allTask").innerHTML = ""
+    }).catch((error) => {
+        console.log(error)
+    })
+
+}
+
+//Comprobamos si ya hay un usuario logueado
+
+
--- main.js
+++ main.js
@@ -3,10 +3,8 @@ let selected = 0
 let i = 1
 
 import { getUUID } from "./utils.js";
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-app.js";
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-auth.js";
+import { login, logOut, auth, user } from "./auth.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-auth.js";
 import {
     getFirestore,
     getDocs,
@@ -17,30 +15,8 @@ import {
     updateDoc,
 } from 'https://www.gstatic.com/firebasejs/9.16.0/firebase-firestore.js'
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+let currentUser = user
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyB6MPkwmFCZQlWcXbDy5zYECJCZV6429dc",
-    authDomain: "todo-list-juan.firebaseapp.com",
-    projectId: "todo-list-juan",
-    storageBucket: "todo-list-juan.appspot.com",
-    messagingSenderId: "290993083560",
-    appId: "1:290993083560:web:d908e94d5bb8d1478d1e9d"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app)
-const provider = new GoogleAuthProvider()
-
-let currentUser
-
-auth.languageCode = "es"
-
-
-//Comprobamos si ya hay un usuario logueado
 onAuthStateChanged(auth, (user) => {
     if (user) {
         currentUser = user
@@ -51,6 +27,8 @@ onAuthStateChanged(auth, (user) => {
         console.log("No hay usuario logueado")
     }
 })
+
+
 //Inicializamos si ya esta logueado
 function init() {
     document.getElementById("boton-login").classList.replace("flex", "hidden")
@@ -61,48 +39,6 @@ function init() {
         <span class = "ml-2 text-white">${currentUser.displayName}</span>
     `
 }
-//Funcion de login
-async function login() {
-    try {
-        currentUser = await signInWithPopup(auth, provider).then((result) => {
-            const credential = GoogleAuthProvider.credentialFromResult(result);
-            const token = credential.accessToken;
-            // The signed-in user info.
-            const user = result.user;
-            document.getElementById("boton-login").classList.replace("flex", "hidden")
-            document.getElementById("boton-logout").classList.replace("hidden", "flex")
-            return user
-        }).catch((error) => {
-            // Handle Errors here.
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            // The email of the user's account used.
-            const email = error.customData.email;
-            // The AuthCredential type that was used.
-            const credential = GoogleAuthProvider.credentialFromError(error);
-            // ...
-        });
-
-    } catch (error) {
-        throw new Error(error)
-    }
-}
-//Funcion de logout
-function logOut() {
-
-    signOut(auth).then(() => {
-        currentUser = ""
-        console.log("LogOut succesful")
-        document.getElementById("user").innerHTML = ""
-        document.getElementById("boton-logout").classList.replace("flex", "hidden")
-        document.getElementById("boton-login").classList.replace("hidden", "flex")
-        document.getElementById("allTask").innerHTML = ""
-    }).catch((error) => {
-        console.log(error)
-    })
-
-}
-
 const db = getFirestore();
 
 let todos = []
